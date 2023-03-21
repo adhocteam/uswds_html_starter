4cce78a886b7c59445bae97d581770a05ead71b1
Bill Mill <bill@billmill.org>
2023-03-21 09:28:52 -0400

    watch for HTML chanages too

--- turbowatch.ts
+++ turbowatch.ts
@@ -1,11 +1,16 @@
 import { watch } from "turbowatch";
 
-// Watch for changes to *.scss and rebuild the css
+// Watch for changes to *.{scss, html} and rebuild the css. We need to watch
+// for changes on the HTML too because it affects what CSS gets purged
 void watch({
   project: __dirname,
   triggers: [
     {
-      expression: ["match", "*.scss", "basename"],
+      expression: [
+        "anyof",
+        ["match", "**/*.scss", "basename"],
+        ["match", "**/*.html", "basename"],
+      ],
       name: "build",
       onChange: async ({ spawn }) => {
         await spawn`npm run build`;
